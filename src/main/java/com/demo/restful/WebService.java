06ad82b84cb30724ed8a4a5a47cc1bb6dc617676
Carlos Cubur <carlos.cubur.cc@gmail.com>
2017-10-08 19:07:29 -0600

    Carlos Cubur - Agregue Cambios

--- src/main/java/com/demo/restful/WebService.java
+++ src/main/java/com/demo/restful/WebService.java
@@ -14,6 +14,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.demo.queue.QueueUtil;
+
 /**
  *
  * @author mmendez
@@ -39,10 +41,37 @@ public class WebService {
     @Path("/enviarDatos")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Persona setDatosPersona(
-            Persona persona
-    ) {
+    public Persona setDatosPersona(Persona persona)
+    {
         persona.setNombre("modificacion");
+        return persona;
+    }
+
+    @POST
+    @Path("/enviarMensaje")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Persona enviarMensaje(Persona persona)
+    {
+        //Envia a cola
+        String nombreCola = "queue.so1.demo";
+        String nombreServicio = "EjemploCola";
+        String serverLocation = "failover:(tcp://localhost:61616)?timeout=3000";
+
+        String message = " {"
+                + " \"platformOrigin\":\"" + "1" + "\","
+                + " \"platformDestiny\":\"" + "2" + "\""
+                + "}";
+        try {
+            QueueUtil.send(nombreCola, true, true, 0, nombreServicio, message, serverLocation);
+
+            System.out.println("Enviando mensaje....");
+            //Thread.sleep(500);
+
+        } catch (Exception e) {
+            System.out.println("Error....");
+            e.printStackTrace();
+        }
         return persona;
     }
 
